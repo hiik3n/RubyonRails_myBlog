a9e3d4e887b162dce09190a0870cebcd0fd4d2db
hiik3n <hiik3n@gmail.com>
2016-10-06 17:59:49 +0700

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
-root to: 'pages#main'
-root 'pages#main' # shortcut for the above
-
 Rails.application.routes.draw do
+	root to: 'pages#main'
+	root 'pages#main' # shortcut for the above
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
